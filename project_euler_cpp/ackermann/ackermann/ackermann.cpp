3f1372d51c0f5f67b430f134c48da1d9131ca74e
Alexandre Maraval <31925705+alexmaraval@users.noreply.github.com>
2017-10-03 16:47:13 +0200

    clearer writing of ackermann.cpp

--- project_euler_cpp/ackermann/ackermann/ackermann.cpp
+++ project_euler_cpp/ackermann/ackermann/ackermann.cpp
@@ -10,20 +10,7 @@
 
 int ack(int m, int n)
 {
-    int ans;
-    
-    if (m == 0)
-    {
-        ans = n+1;
-    }
-    else if (n == 0)
-    {
-        ans = ack(m-1,1);
-    }
-    else
-    {
-        ans = ack(m-1, ack(m,n-1));
-    }
-    
-    return (ans);
+    if      (m == 0)    { return(n+1);                  }
+    else if (n == 0)    { return(ack(m-1,1));           }
+    else                { return(ack(m-1, ack(m,n-1))); }
 }
