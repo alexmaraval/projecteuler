71cce9727f44eb0c641c470e55a6e2148d931ead
Alexandre Maraval <31925705+alexmaraval@users.noreply.github.com>
2017-10-04 15:48:19 +0200

    created pb016 file

---
+++ project_euler_cpp/project_euler_cpp/pb016.hpp
@@ -0,0 +1,14 @@
+//
+//  pb016.hpp
+//  project_euler_cpp
+//
+//  Created by Alexandre Maraval on 04.10.17.
+//  Copyright © 2017 Alexandre Maraval. All rights reserved.
+//
+
+#ifndef pb016_hpp
+#define pb016_hpp
+
+#include <stdio.h>
+
+#endif /* pb016_hpp */
