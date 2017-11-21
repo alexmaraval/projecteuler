ac31997ff3efcd430eba847500fa8191ac247af9
Alexandre Maraval <maraval.alexandre@gmail.com>
2017-11-21 16:46:37 +0100

    created pb020

---
+++ project_euler_cpp/project_euler_cpp/pb020.cpp
@@ -0,0 +1,9 @@
+//
+//  pb020.cpp
+//  project_euler_cpp
+//
+//  Created by Alexandre Maraval on 21.11.17.
+//  Copyright © 2017 Alexandre Maraval. All rights reserved.
+//
+
+#include "pb020.hpp"
---
+++ project_euler_cpp/project_euler_cpp/pb020.hpp
@@ -0,0 +1,14 @@
+//
+//  pb020.hpp
+//  project_euler_cpp
+//
+//  Created by Alexandre Maraval on 21.11.17.
+//  Copyright © 2017 Alexandre Maraval. All rights reserved.
+//
+
+#ifndef pb020_hpp
+#define pb020_hpp
+
+#include <stdio.h>
+
+#endif /* pb020_hpp */
