c9eff394fe135f430c4f5b87302958aa532e7574
Alexandre Maraval <maraval.alexandre@gmail.com>
2017-12-14 11:53:18 +0000

    created pb024

---
+++ project_euler_cpp/project_euler_cpp/pb024.cpp
@@ -0,0 +1,9 @@
+//
+//  pb024.cpp
+//  project_euler_cpp
+//
+//  Created by Alexandre Maraval on 14.12.17.
+//  Copyright © 2017 Alexandre Maraval. All rights reserved.
+//
+
+#include "pb024.hpp"
---
+++ project_euler_cpp/project_euler_cpp/pb024.hpp
@@ -0,0 +1,14 @@
+//
+//  pb024.hpp
+//  project_euler_cpp
+//
+//  Created by Alexandre Maraval on 14.12.17.
+//  Copyright © 2017 Alexandre Maraval. All rights reserved.
+//
+
+#ifndef pb024_hpp
+#define pb024_hpp
+
+#include <stdio.h>
+
+#endif /* pb024_hpp */
